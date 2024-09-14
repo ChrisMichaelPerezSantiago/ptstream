76224db20f24403494b0ea70fb3f57d10eaed582
Chris Michael <chris.perez@upr.edu>
2024-09-14 16:59:47 -0400

    feat: make 'component' and 'icon' props optional in SceneProps type.

--- src/scenes/root/index.tsx
+++ src/scenes/root/index.tsx
@@ -1,21 +1,16 @@
-import React from "react";
 import { useSelector } from "react-redux";
 
 import { RootState } from "../../redux/store";
 import SerieScene from "../../components/scenes/Series";
 import MovieScene from "../../components/scenes/Movies";
 import { Scene, SceneProps } from "../../types";
-import MovieIcon from "../../components/Icons/MovieIcon";
-import TvIcon from "../../components/Icons/TvIcon";
 
 const scenes: Record<Scene, SceneProps> = {
   series: {
     component: <SerieScene />,
-    icon: TvIcon,
   },
   movies: {
     component: <MovieScene />,
-    icon: MovieIcon,
   },
 };
 
