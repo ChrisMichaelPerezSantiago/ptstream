374d1418adc290c9b1adb74545c63a18a726d581
Chris Michael <chris.perez@upr.edu>
2025-01-24 21:44:17 -0400

    update(PlyrVideoPlayer/index.tsx): integrate Effect library for functional programming

--- src/components/PlyrVideoPlayer/index.tsx
+++ src/components/PlyrVideoPlayer/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Plyr from "plyr-react";
 import { X } from "lucide-react";
 import { map, toLower } from "lodash";
+import { Effect, pipe } from "effect";
 
 import { PromoResult } from "../../types";
 
@@ -18,33 +19,45 @@ export function PlyrVideoPlayer({
 }: PlyrVideoPlayerProps) {
   const [isFloating, setIsFloating] = useState(true);
 
-  const sources = map(
-    promo,
-    ({ key, site, size, type }) =>
-      ({
-        src: key,
-        provider: toLower(site),
-        size: size,
-        type: type,
-      } as Plyr.Source)
+  const sources = pipe(
+    Effect.sync(() => promo),
+    Effect.map((values) =>
+      map(
+        values,
+        ({ key, site, size, type }) =>
+          ({
+            src: key,
+            provider: toLower(site),
+            size: size,
+            type: type,
+          } as Plyr.Source)
+      )
+    ),
+    Effect.runSync
   );
+
+  const handleClose = () =>
+    pipe(
+      Effect.sync(() => {
+        onClosePlayer();
+        setIsFloating(false);
+      }),
+      Effect.runSync
+    );
 
   return (
     <div
       className={`${
         isFloating
-          ? "fixed bottom-4 right-10 w-1/3 z-50 animate-pip-drop-effect"
-          : "relative w-3/4 mx-auto"
+          ? "fixed bottom-4 right-10 z-50 w-1/3 animate-pip-drop-effect"
+          : "relative mx-auto w-3/4"
       } overflow-hidden rounded-lg aspect-w-16 aspect-h-9`}
     >
       {/* Close Button */}
       {isFloating && (
         <button
-          className="absolute z-10 p-2 bg-transparent border-none rounded-full cursor-pointer top-2 right-2"
-          onClick={() => {
-            onClosePlayer();
-            setIsFloating(false);
-          }}
+          className="absolute top-2 right-2 z-10 p-2 bg-transparent rounded-full border-none cursor-pointer"
+          onClick={handleClose}
         >
           <X color="white" />
         </button>
